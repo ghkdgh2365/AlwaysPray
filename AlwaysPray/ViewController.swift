4c5f58a81840d78a67ff984dcc1dbb039d9db940
hohwang <ghkdgh2365@naver.com>
2020-01-14 21:11:46 +0900

    Initial Commit

---
+++ AlwaysPray/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlwaysPray
+//
+//  Created by Ho Hwang on 14/01/2020.
+//  Copyright © 2020 hohwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
