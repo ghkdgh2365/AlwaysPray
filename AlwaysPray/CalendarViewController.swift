c51f7056573564c22f2d421e5b05fbf80f0af09b
hohwang <hohwang@mintech.kr>
2020-08-15 22:14:40 +0900

    before first deploy

---
+++ AlwaysPray/CalendarViewController.swift
@@ -0,0 +1,91 @@
+//
+//  CalendarViewController.swift
+//  AlwaysPray
+//
+//  Created by Ho Hwang on 2020/08/01.
+//  Copyright © 2020 hohwang. All rights reserved.
+//
+import UIKit
+import CoreData
+import Foundation
+import FSCalendar
+
+class CalendarViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
+    
+    @IBOutlet var calendar: FSCalendar!
+    
+    var prayItems = [Pray]()
+    
+    var moc:NSManagedObjectContext!
+    
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
+    lazy var dateFormatter1: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter
+    }()
+    
+    var fillDefaultColors = Dictionary<String, UIColor>();
+    var titleDefaultColors : Array = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        moc = appDelegate?.persistentContainer.viewContext
+        
+        calendar.dataSource = self
+        calendar.delegate = self
+        
+        loadData()
+        
+    }
+    
+    func loadData(){
+        let prayRequest:NSFetchRequest<Pray> = Pray.fetchRequest()
+        
+        let sortDescriptor = NSSortDescriptor(key: "added", ascending: false)
+        prayRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            try prayItems = moc.fetch(prayRequest)
+            prayItems.forEach { (pray) in
+                self.fillDefaultColors[self.dateFormatter1.string(from: pray.added!)] = UIColor(red: 187/255, green: 10/255, blue: 30/255, alpha: 1)
+                self.titleDefaultColors.append(self.dateFormatter1.string(from: pray.added!))
+            }
+        } catch {
+            print("Could not load data.")
+        }
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("selected")
+    }
+    
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, fillDefaultColorFor date: Date) -> UIColor? {
+        let key = self.dateFormatter1.string(from: date)
+        if let color = self.fillDefaultColors[key] {
+            return color
+        }
+        return nil
+    }
+    
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        let dateString : String = dateFormatter1.string(from:date)
+        if self.titleDefaultColors.contains(dateString) {
+            return .white
+        } else {
+            return nil
+        }
+    }
+    
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, borderDefaultColorFor date: Date) -> UIColor? {
+        var borderDefaultColors = Dictionary<String, UIColor>();
+        borderDefaultColors[self.dateFormatter1.string(from: Date())] = UIColor.blue
+        let key = self.dateFormatter1.string(from: date)
+        if let color = borderDefaultColors[key] {
+            return color
+        }
+        return appearance.borderDefaultColor
+    }
+    
+}
